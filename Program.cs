f972fd86f4aa238d1808cc3a226b53d3a65b25d9
Volkmar Rigo <volkmar@rigo.bz>
2021-11-13 12:23:10 +0100

    Refactoring
    - Dotnet 6
    - Global usings
    - Filewide namespaces

--- Program.cs
+++ Program.cs
@@ -1,27 +1,23 @@
-﻿using AdventOfCode.Base;
-using System;
+﻿namespace AdventOfCode;
 
-namespace AdventOfCode
+class Program
 {
-    class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        while (true)
         {
-            while (true)
+
+            Console.WriteLine("Which day do you want to initialize? Press enter to cancel.");
+            var dayString = Console.ReadLine();
+            if (string.IsNullOrEmpty(dayString))
+                return;
+
+            if (int.TryParse(dayString, out var day))
             {
-
-                Console.WriteLine("Which day do you want to initialize? Press enter to cancel.");
-                var dayString = Console.ReadLine();
-                if (string.IsNullOrEmpty(dayString))
-                    return;
-
-                if (int.TryParse(dayString, out var day))
-                {
-                    DayInitialization.Execute(day);
-                    return;
-                }
+                DayInitialization.Execute(day);
+                return;
             }
-
         }
+
     }
 }
